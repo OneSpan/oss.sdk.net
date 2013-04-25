4cda81e76d7d2a9a1dc044cc9ca152078015d8ea
unknown <lhsieh@ESIGNLIVE707.silanis.com>
2013-04-25 14:21:15 -0400

    added project file and started documentation

--- CSharp_SDK/Silanis.ESL.SDK/EslClient.cs
+++ CSharp_SDK/Silanis.ESL.SDK/EslClient.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Silanis.ESL.SDK
 {
+    /// <summary>
+    /// The EslClient acts as a E-SignLive client.
+    /// The EslClient has access to service classes that help create packages and retrieve resources from the client's account.
+    /// </summary>
 	public class EslClient
 	{
 
@@ -12,6 +16,12 @@ namespace Silanis.ESL.SDK
 		private FieldSummaryService fieldSummaryService;
 		private AuditService auditService;
 
+        /// <summary>
+        /// EslClient constructor.
+        /// Initiates service classes that can be used by the client.
+        /// </summary>
+        /// <param name="apiToken">The client's api token.</param>
+        /// <param name="baseUrl">The staging or production url.</param>
 		public EslClient (string apiToken, string baseUrl)
 		{
 			this.apiToken = apiToken;
@@ -21,37 +31,55 @@ namespace Silanis.ESL.SDK
 			fieldSummaryService = new FieldSummaryService (apiToken, baseUrl);
 			auditService = new AuditService (apiToken, baseUrl);
 		}
-
+        
+        /// <summary>
+        /// ApiToken property
+        /// </summary>
 		public string ApiToken {
 			get {
 				return this.apiToken;
 			}
 		}
 
+        /// <summary>
+        /// BaseUrl property
+        /// </summary>
 		public string BaseUrl {
 			get {
 				return this.baseUrl;
 			}
 		}
 
+        /// <summary>
+        /// PackageService property
+        /// </summary>
 		public PackageService PackageService {
 			get {
 				return this.packageService;
 			}
 		}
 
+        /// <summary>
+        /// SessionService property
+        /// </summary>
 		public SessionService SessionService {
 			get {
 				return this.sessionService;
 			}
 		}
 
+        /// <summary>
+        /// FieldSummaryService property
+        /// </summary>
 		public FieldSummaryService FieldSummaryService {
 			get {
 				return this.fieldSummaryService;
 			}
 		}
 
+        /// <summary>
+        /// AuditService property
+        /// </summary>
 		public AuditService AuditService {
 			get {
 				return this.auditService;
