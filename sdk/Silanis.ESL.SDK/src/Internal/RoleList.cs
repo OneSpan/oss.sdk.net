c2d48fb03866297f820e28a5884835b880fecb49
Etienne Hardy <etienne.hardy@gmail.com>
2013-06-04 14:08:13 -0400

    Moved source files into separate directories. Also added a command line project for samples

---
+++ sdk/Silanis.ESL.SDK/src/Internal/RoleList.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Silanis.ESL.SDK
+{
+	/// <summary>
+	/// For internal use.
+	/// </summary>
+	public class RoleList
+	{
+		[JsonProperty("results")]
+		public List<Role> Roles { get; private set; }
+
+		[JsonProperty("count")]
+		public int Count { get; private set; }
+
+	}
+}
+
