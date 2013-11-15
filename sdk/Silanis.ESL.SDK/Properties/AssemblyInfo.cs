9efd5b5e2ae749aa9ee6f55170bf0619081557d1
ehardy <etienne_hardy@silanis.com>
2013-11-15 10:08:56 -0500

    Added a signing key to the DLL properties of the SDK. This allows to install the SDK in the GAC.

--- sdk/Silanis.ESL.SDK/Properties/AssemblyInfo.cs
+++ sdk/Silanis.ESL.SDK/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("9.7.*")]
-
+[assembly: AssemblyKeyFileAttribute("sdk.snk")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
