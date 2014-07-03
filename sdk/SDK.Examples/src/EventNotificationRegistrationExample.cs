27fe91fa79df544a59a96fd50ce611d76a97557f
Dave Lawson <davelawson@hotmail.com>
2014-07-03 10:56:22 -0400

    refactored account service into two layers

--- sdk/SDK.Examples/src/EventNotificationRegistrationExample.cs
+++ sdk/SDK.Examples/src/EventNotificationRegistrationExample.cs
@@ -33,9 +33,21 @@ namespace SDK.Examples
 		{
 			// Register for event notification
 			eslClient.EventNotificationService.Register(EventNotificationConfigBuilder.NewEventNotificationConfig(URL)
-				.ForEvent(EVENT1)
-				.ForEvent(EVENT2)
-				.ForEvent(EVENT3));
+                .ForEvent(NotificationEvent.DOCUMENT_SIGNED)
+                .ForEvent(NotificationEvent.PACKAGE_ACTIVATE)
+                .ForEvent(NotificationEvent.PACKAGE_COMPLETE)
+                .ForEvent(NotificationEvent.PACKAGE_CREATE)
+                .ForEvent(NotificationEvent.PACKAGE_DEACTIVATE)
+                .ForEvent(NotificationEvent.PACKAGE_DECLINE)
+                .ForEvent(NotificationEvent.PACKAGE_DELETE)
+                .ForEvent(NotificationEvent.PACKAGE_EXPIRE)
+                .ForEvent(NotificationEvent.PACKAGE_OPT_OUT)
+                .ForEvent(NotificationEvent.PACKAGE_READY_FOR_COMPLETION)
+                .ForEvent(NotificationEvent.PACKAGE_RESTORE)
+                .ForEvent(NotificationEvent.PACKAGE_TRASH));
+//				.ForEvent(EVENT1)
+//				.ForEvent(EVENT2)
+//				.ForEvent(EVENT3));
 
 			// Get the registered event notifications
 			eventNotificationConfig = eslClient.EventNotificationService.GetEventNotificationConfig();
