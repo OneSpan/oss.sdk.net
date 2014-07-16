848204410c349e8cb1365ebc7e11a740d758adcc
Chi-Wing Sit <chiwingsit@gmail.com>
2014-07-16 14:52:46 -0400

    esl-9780, esl-9781 Add Approval and Modify Approval calls

--- sdk/Silanis.ESL.SDK/src/Services/ApprovalService.cs
+++ sdk/Silanis.ESL.SDK/src/Services/ApprovalService.cs
@@ -1,6 +1,7 @@
 using System;
 using Silanis.ESL.SDK.Internal;
 using Newtonsoft.Json;
+using Silanis.ESL.API;
 
 namespace Silanis.ESL.SDK
 {
@@ -13,9 +14,90 @@ namespace Silanis.ESL.SDK
             this.apiClient = apiClient;
         }
 
-        public void DeleteApproval(PackageId packageId, string documentId, string approvalId){
-        
+        public void DeleteApproval(PackageId packageId, string documentId, string approvalId)
+        {
             apiClient.DeleteApproval( packageId.Id, documentId, approvalId );
+        }
+
+        public string AddApproval(DocumentPackage sdkPackage, string documentId, Signature signature)
+        {
+            Approval approval = new SignatureConverter(signature).ToAPIApproval();
+            Package apiPackage = new DocumentPackageConverter(sdkPackage).ToAPIPackage();
+
+            if (signature.IsPlaceholderSignature())
+            {
+                approval.Role = signature.RoleId.Id;
+            }
+            else if (signature.IsGroupSignature())
+            {
+                approval.Role = FindRoleIdForGroup(signature.GroupId, apiPackage);
+            }
+            else
+            {
+                approval.Role = FindRoleIdForSigner(signature.SignerEmail, apiPackage);
+            }
+
+            return apiClient.AddApproval(sdkPackage.Id, documentId, approval);
+        }
+
+        public void ModifyApproval(DocumentPackage sdkPackage, string documentId, string approvalId, Signature signature)
+        {
+            Approval approval = new SignatureConverter(signature).ToAPIApproval();
+            Package apiPackage = new DocumentPackageConverter(sdkPackage).ToAPIPackage();
+
+            if (signature.IsPlaceholderSignature())
+            {
+                approval.Role = signature.RoleId.Id;
+            }
+            else if (signature.IsGroupSignature())
+            {
+                approval.Role = FindRoleIdForGroup(signature.GroupId, apiPackage);
+            }
+            else
+            {
+                approval.Role = FindRoleIdForSigner(signature.SignerEmail, apiPackage);
+            }
+
+            apiClient.ModifyApproval(sdkPackage.Id, documentId, approvalId, approval);
+        }
+
+        public Signature GetApproval(DocumentPackage sdkPackage, string documentId, string approvalId)
+        {
+            Approval approval = apiClient.GetApproval(sdkPackage.Id, documentId, approvalId);
+            Package aPackage = new DocumentPackageConverter(sdkPackage).ToAPIPackage();
+            return new SignatureConverter(approval, aPackage).ToSDKSignature();
+        }
+
+        private string FindRoleIdForGroup(GroupId groupId, Silanis.ESL.API.Package createdPackage)
+        {
+            foreach (Silanis.ESL.API.Role role in createdPackage.Roles)
+            {
+                if (role.Signers.Count > 0 && role.Signers[0].Group != null)
+                {
+                    if (groupId.Id.Equals(role.Signers[0].Group.Id))
+                    {
+                        return role.Id;
+                    }
+                }
+            }
+
+            throw new EslException(String.Format("No Role found for group with id {0}", groupId.Id), null);
+        }
+
+        private string FindRoleIdForSigner(string signerEmail, Silanis.ESL.API.Package createdPackage)
+        {
+            foreach (Silanis.ESL.API.Role role in createdPackage.Roles)
+            {
+                if (role.Signers.Count > 0 && role.Signers[0].Email != null)
+                {
+                    if (signerEmail.Equals(role.Signers[0].Email))
+                    {
+                        return role.Id;
+                    }
+                }
+            }
+
+            throw new EslException(String.Format("No Role found for signer email {0}", signerEmail), null);
         }
     }
 }
--- sdk/Silanis.ESL.SDK/src/Services/Internal/ApprovalApiClient.cs
+++ sdk/Silanis.ESL.SDK/src/Services/Internal/ApprovalApiClient.cs
@@ -1,6 +1,7 @@
 using System;
 using Newtonsoft.Json;
 using Silanis.ESL.SDK.Internal;
+using Silanis.ESL.API;
 
 namespace Silanis.ESL.SDK
 {
@@ -8,11 +9,13 @@ namespace Silanis.ESL.SDK
     {
         private UrlTemplate template;
         private RestClient restClient;
+        private JsonSerializerSettings jsonSettings;
 
-        public ApprovalApiClient(RestClient restClient, string baseUrl)
+        public ApprovalApiClient(RestClient restClient, string baseUrl, JsonSerializerSettings jsonSettings)
         {
             this.restClient = restClient;
             template = new UrlTemplate (baseUrl);
+            this.jsonSettings = jsonSettings;
         }        
         
         public void DeleteApproval(string packageId, string documentId, string approvalId)
@@ -30,6 +33,68 @@ namespace Silanis.ESL.SDK
             }
             catch (Exception e) {
                 throw new EslException("Could not delete signature.\t" + " Exception: " + e.Message, e);
+            }
+        }
+
+        public string AddApproval(PackageId packageId, string documentId, Approval approval)
+        {
+            string path = template.UrlFor(UrlTemplate.APPROVAL_PATH)
+                .Replace("{packageId}", packageId.Id)
+                    .Replace("{documentId}", documentId)
+                    .Build();
+
+            try {
+                string json = JsonConvert.SerializeObject (approval, jsonSettings);
+                string response = restClient.Post(path, json);
+                Silanis.ESL.API.Approval apiApproval = JsonConvert.DeserializeObject<Silanis.ESL.API.Approval> (response, jsonSettings);
+                return apiApproval.Id;
+            }
+            catch (EslServerException e) {
+                throw new EslServerException("Could not add signature.\t" + " Exception: " + e.Message, e.ServerError, e);
+            }
+            catch (Exception e) {
+                throw new EslException("Could not add signature.\t" + " Exception: " + e.Message, e);
+            }
+        }
+
+        public void ModifyApproval(PackageId packageId, string documentId, string approvalId, Approval approval)
+        {
+            string path = template.UrlFor(UrlTemplate.APPROVAL_ID_PATH)
+                .Replace("{packageId}", packageId.Id)
+                    .Replace("{documentId}", documentId)
+                    .Replace("{approvalId}", approvalId)
+                    .Build();
+
+            try {
+                string json = JsonConvert.SerializeObject (approval, jsonSettings);
+                restClient.Put(path, json);
+            }
+            catch (EslServerException e) {
+                throw new EslServerException("Could not add signature.\t" + " Exception: " + e.Message, e.ServerError, e);
+            }
+            catch (Exception e) {
+                throw new EslException("Could not add signature.\t" + " Exception: " + e.Message, e);
+            }
+        }
+
+        public Approval GetApproval(PackageId packageId, string documentId, string approvalId)
+        {
+            string path = template.UrlFor(UrlTemplate.APPROVAL_ID_PATH)
+                .Replace("{packageId}", packageId.Id)
+                    .Replace("{documentId}", documentId)
+                    .Replace("{approvalId}", approvalId)
+                    .Build();
+
+            try {
+                string response = restClient.Get(path);
+                Silanis.ESL.API.Approval apiApproval = JsonConvert.DeserializeObject<Silanis.ESL.API.Approval> (response, jsonSettings);
+                return apiApproval;
+            }
+            catch (EslServerException e) {
+                throw new EslServerException("Could not add signature.\t" + " Exception: " + e.Message, e.ServerError, e);
+            }
+            catch (Exception e) {
+                throw new EslException("Could not add signature.\t" + " Exception: " + e.Message, e);
             }
         }
 
