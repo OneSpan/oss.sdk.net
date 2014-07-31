5a9c70d52b8d0801c9ec18a987354b707014ff3c
Lena Hsieh <lena_hsieh@silanis.com>
2014-07-31 11:32:05 -0400

    Increased the page result

--- sdk/SDK.Examples/src/SenderManipulationExample.cs
+++ sdk/SDK.Examples/src/SenderManipulationExample.cs
@@ -75,7 +75,7 @@ namespace SDK.Examples
 
             eslClient.AccountService.DeleteSender(createdSender2.Id);
 
-            accountMembersWithDeletedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 100));
+            accountMembersWithDeletedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 500));
 
             updatedSenderInfo = SenderInfoBuilder.NewSenderInfo(email3)
                 .WithName("updatedFirstName", "updatedLastName")
@@ -84,7 +84,7 @@ namespace SDK.Examples
                     .Build();
 
             eslClient.AccountService.UpdateSender(updatedSenderInfo, accountMembersWithDeletedSender[email3].Id);
-            accountMembersWithUpdatedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 100));
+            accountMembersWithUpdatedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 500));
         }
     }
 }
