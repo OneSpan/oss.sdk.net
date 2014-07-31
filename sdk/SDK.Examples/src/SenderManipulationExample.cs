5c195b4b6d97b6aa5560be50ffd843c7614b3728
Lena Hsieh <lena_hsieh@silanis.com>
2014-07-31 10:18:37 -0400

    Fix SenderManipulationExampleTest

--- sdk/SDK.Examples/src/SenderManipulationExample.cs
+++ sdk/SDK.Examples/src/SenderManipulationExample.cs
@@ -71,11 +71,11 @@ namespace SDK.Examples
 
             eslClient.AccountService.SendInvite(createdSender1.Id);
 
-            accountMembers = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 100));
+            accountMembers = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(0, -1));
 
             eslClient.AccountService.DeleteSender(createdSender2.Id);
 
-            accountMembersWithDeletedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 100));
+            accountMembersWithDeletedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(0, -1));
 
             updatedSenderInfo = SenderInfoBuilder.NewSenderInfo(email3)
                 .WithName("updatedFirstName", "updatedLastName")
@@ -84,7 +84,7 @@ namespace SDK.Examples
                     .Build();
 
             eslClient.AccountService.UpdateSender(updatedSenderInfo, accountMembersWithDeletedSender[email3].Id);
-            accountMembersWithUpdatedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(1, 100));
+            accountMembersWithUpdatedSender = eslClient.AccountService.GetSenders(Direction.ASCENDING, new PageRequest(0, -1));
         }
     }
 }
