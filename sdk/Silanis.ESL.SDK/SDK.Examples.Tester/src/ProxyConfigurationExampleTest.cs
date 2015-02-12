600283fc68de3cbd34642e705569c66b07c1fb6e
Seung Choi <seung_choi@silanis.com>
2015-02-12 11:56:41 -0500

    sdk-1183. Changed ProxyConfigurationExampleTest

--- sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/ProxyConfigurationExampleTest.cs
+++ sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/ProxyConfigurationExampleTest.cs
@@ -3,7 +3,6 @@ using NUnit.Framework;
 using Silanis.ESL.SDK;
 using NUnit.Framework.SyntaxHelpers;
 using System.Collections.Generic;
-using TrotiNet;
 
 namespace SDK.Examples
 {
@@ -12,26 +11,20 @@ namespace SDK.Examples
         [Test]
         public void verifyResult()
         {
-            ExecutionByHttpProxy();
-        }
-
-        public void ExecutionByHttpProxy(){
-
             ProxyConfigurationExample example = new ProxyConfigurationExample(Props.GetInstance());
+
             example.ExecuteViaHttpProxy();
             DocumentPackage documentPackage1 = example.eslClientWithHttpProxy.GetPackage(example.PackageId);
-            Assert.AreEqual(documentPackage1.Id.Id, example.PackageId.Id);
+            DocumentPackage documentPackage2 = example.EslClient.GetPackage(example.PackageId);
+            Assert.AreEqual(example.PackageId.Id, documentPackage1.Id.Id);
+            Assert.AreEqual(example.PackageId.Id, documentPackage2.Id.Id);
 
-        }
-
-        public void ExecutionByHttpProxyWithCredentials(){
-
-            ProxyConfigurationExample example = new ProxyConfigurationExample(Props.GetInstance());
             example.ExecuteViaHttpProxyWithCredentials();
-            DocumentPackage documentPackage2 = example.eslClientWithHttpProxyHasCredentials.GetPackage(example.PackageId);
-            Assert.AreEqual(documentPackage2.Id.Id, example.PackageId.Id);
+            DocumentPackage documentPackage3 = example.eslClientWithHttpProxyHasCredentials.GetPackage(example.PackageId);
+            DocumentPackage documentPackage4 = example.EslClient.GetPackage(example.PackageId);
+            Assert.AreEqual(example.PackageId.Id, documentPackage3.Id.Id);
+            Assert.AreEqual(example.PackageId.Id, documentPackage4.Id.Id);
         }
-
     }
 }
 
