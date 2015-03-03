d3413199579e7786a55e16ae67654a4560eb2ce3
Seung Choi <seung_choi@silanis.com>
2015-03-03 14:04:29 -0500

    esl-14693. Added GettingStartedExample for documentation

---
+++ sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/GettingStartedExampleTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using System;
+
+namespace SDK.Examples
+{
+    [TestFixture()]
+    public class GettingStartedExampleTest
+    {
+        [Test()]
+        public void VerifyResult()
+        {
+            GettingStartedExample example = new GettingStartedExample(Props.GetInstance());
+            example.Run();
+
+        }
+    }
+}
+
