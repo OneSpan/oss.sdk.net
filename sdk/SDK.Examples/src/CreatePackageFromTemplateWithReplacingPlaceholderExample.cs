cd5339f7bd7cd4747ff3ca22524d75611136b60d
Seung Choi <seung_choi@silanis.com>
2015-03-10 12:31:57 -0400

    sdk-1189. Added example about replacement of placeholder (CreatePackageFromTemplateWithReplacingPlaceholderExample, CreatePackageFromTemplateWithReplacingPlaceholderExampleTest) for documentation

---
+++ sdk/SDK.Examples/src/CreatePackageFromTemplateWithReplacingPlaceholderExample.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IO;
+using Silanis.ESL.SDK;
+using Silanis.ESL.SDK.Builder;
+
+namespace SDK.Examples
+{
+    public class CreatePackageFromTemplateWithReplacingPlaceholderExample : SDKSample
+    {
+        public static void Main(string[] args)
+        {
+            new CreatePackageFromTemplateWithReplacingPlaceholderExample(Props.GetInstance()).Run();
+        }
+
+        private Stream fileStream1;
+
+        public string email1;
+        public string email2;
+        public PackageId templateId;
+
+        public readonly string DOCUMENT_NAME = "First Document";
+        public readonly string DOCUMENT_ID = "doc1";
+        public readonly string TEMPLATE_NAME = "CreatePackageFromTemplateWithReplacingPlaceholderExample Template: " + DateTime.Now;
+        public readonly string TEMPLATE_DESCRIPTION = "This is a template created using the e-SignLive SDK";
+        public readonly string TEMPLATE_EMAIL_MESSAGE = "This message should be delivered to all signers";
+        public readonly string TEMPLATE_SIGNER_FIRST = "John";
+        public readonly string TEMPLATE_SIGNER_LAST = "Smith";
+
+        public readonly string PACKAGE_NAME = "CreatePackageFromTemplateWithReplacingPlaceholderExample Package: " + DateTime.Now;
+        public readonly string PACKAGE_DESCRIPTION = "This is a package created using the e-SignLive SDK";
+        public readonly string PACKAGE_EMAIL_MESSAGE = "This message should be delivered to all signers";
+        public readonly string PACKAGE_SIGNER_FIRST = "Patty";
+        public readonly string PACKAGE_SIGNER_LAST = "Galant";
+
+        public readonly string PLACEHOLDER_ID = "PlaceholderId1";
+
+        public CreatePackageFromTemplateWithReplacingPlaceholderExample(Props props) : this(props.Get("api.key"), props.Get("api.url"), props.Get("1.email"), props.Get("2.email") )
+        {
+        }
+
+        public CreatePackageFromTemplateWithReplacingPlaceholderExample(string apiKey, string apiUrl, string email1, string email2) : base( apiKey, apiUrl )
+        {
+            this.email1 = email1;
+            this.email2 = email2;
+            this.fileStream1 = File.OpenRead(new FileInfo(Directory.GetCurrentDirectory() + "/src/document.pdf").FullName);
+        }
+
+        override public void Execute()
+        {
+            DocumentPackage template = PackageBuilder.NewPackageNamed(TEMPLATE_NAME)
+                .DescribedAs(TEMPLATE_DESCRIPTION)
+                    .WithEmailMessage(TEMPLATE_EMAIL_MESSAGE)
+                    .WithSigner(SignerBuilder.NewSignerWithEmail(email1)
+                                .WithFirstName(TEMPLATE_SIGNER_FIRST)
+                                .WithLastName(TEMPLATE_SIGNER_LAST))
+                    .WithSigner(SignerBuilder.NewSignerPlaceholder(new Placeholder(PLACEHOLDER_ID)))
+                    .WithDocument(DocumentBuilder.NewDocumentNamed(DOCUMENT_NAME)
+                                  .WithId(DOCUMENT_ID)
+                                  .FromStream(fileStream1, DocumentType.PDF)
+                                  .WithSignature(SignatureBuilder.SignatureFor(email1)
+                                   .OnPage(0)
+                                   .AtPosition(100, 100))
+                                  .WithSignature(SignatureBuilder.SignatureFor(new Placeholder(PLACEHOLDER_ID))
+                                   .OnPage(0)
+                                   .AtPosition(400, 100)))
+                    .Build();
+
+            templateId = eslClient.CreateTemplate(template);
+
+            DocumentPackage newPackage = PackageBuilder.NewPackageNamed(PACKAGE_NAME)
+                .DescribedAs(PACKAGE_DESCRIPTION)
+                    .WithEmailMessage(PACKAGE_EMAIL_MESSAGE)
+                    .WithSigner(SignerBuilder.NewSignerWithEmail(email2)
+                                .WithFirstName(PACKAGE_SIGNER_FIRST)
+                                .WithLastName(PACKAGE_SIGNER_LAST).Replacing(new Placeholder(PLACEHOLDER_ID)))
+                    .WithSettings(DocumentPackageSettingsBuilder.NewDocumentPackageSettings().WithInPerson())
+                    .Build();
+
+            packageId = eslClient.CreatePackageFromTemplate(templateId, newPackage);
+            retrievedPackage = eslClient.GetPackage(packageId);
+        }
+    }
+}
+
---
+++ sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/CreatePackageFromTemplateWithReplacingPlaceholderExampleTest.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using System;
+using Silanis.ESL.SDK;
+using System.Collections.Generic;
+
+namespace SDK.Examples
+{
+    [TestFixture()]
+    public class CreatePackageFromTemplateWithReplacingPlaceholderExampleTest
+    {
+        [Test()]
+        public void VerifyResult()
+        {
+            CreatePackageFromTemplateWithReplacingPlaceholderExample example = new CreatePackageFromTemplateWithReplacingPlaceholderExample(Props.GetInstance());
+            example.Run();
+
+            Assert.AreEqual(3, example.RetrievedPackage.Signers.Count);
+            Assert.AreEqual(0, example.RetrievedPackage.Placeholders.Count);
+
+            Signer signer1 = example.RetrievedPackage.Signers[example.email1];
+            Signer signer2 = example.RetrievedPackage.Signers[example.email2];
+
+            Assert.AreEqual(example.TEMPLATE_SIGNER_FIRST, signer1.FirstName);
+            Assert.AreEqual(example.TEMPLATE_SIGNER_LAST, signer1.LastName);
+            Assert.AreEqual(example.PACKAGE_SIGNER_FIRST, signer2.FirstName);
+            Assert.AreEqual(example.PACKAGE_SIGNER_LAST, signer2.LastName);
+            Assert.AreEqual(example.PLACEHOLDER_ID, signer2.Id);
+
+
+            List<Signature> signatures = example.RetrievedPackage.Documents[example.DOCUMENT_NAME].Signatures;
+
+            Assert.AreEqual(2, signatures.Count);
+            Assert.AreEqual(example.email1, signatures[0].SignerEmail);
+            Assert.AreEqual(example.email2, signatures[1].SignerEmail);
+        }
+    }
+}
+
