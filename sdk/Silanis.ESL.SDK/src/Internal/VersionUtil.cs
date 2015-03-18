c07e9d9ecd9660c3c9eb2b72728cd2aebeb3419c
Seung Choi <seung_choi@silanis.com>
2015-03-18 11:24:36 -0400

    Added VersionUtil

---
+++ sdk/Silanis.ESL.SDK/src/Internal/VersionUtil.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Silanis.ESL.SDK
+{
+    public class VersionUtil
+    {
+        public static string getVersion() {
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            string version = String.Format("{0}.{1}", fileVersionInfo.ProductMajorPart, fileVersionInfo.ProductMinorPart);
+
+            if (fileVersionInfo.ProductBuildPart != 0) 
+            {
+                version = version + "." + fileVersionInfo.ProductBuildPart;
+            }
+            return version;
+        }
+    }
+}
+
