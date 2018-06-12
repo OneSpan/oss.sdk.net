marcinsliwinskivasco/esl.sdk.net

PB-11324: Added support for optional signatures.

sdk/SDK.Examples/src/SignableOptionalSignaturesExample.cs
@@ -1,0 +1,65 @@
+using System;
+using System.IO;
+using Silanis.ESL.SDK;
+using System.Collections.Generic;
+using Silanis.ESL.SDK.Builder;
+
+namespace SDK.Examples
+{
+    public class SignableOptionalSignaturesExample : SDKSample
+    {
+        public static void Main(string[] args)
+        {
+            new SignableOptionalSignaturesExample().Run();
+        }
+
+        public DocumentPackage sentPackage;
+
+        private string signer1Id = "signer1Id";
+        private string signer2Id = "signer2Id";
+        private string documentId = "documentId";
+        private string DOCUMENT_NAME = "First Document";
+
+        public IList<Signature> signer1SignableSignatures, signer2SignableSignatures;
+
+        override public void Execute()
+        {
+            DocumentPackage superDuperPackage = PackageBuilder.NewPackageNamed(PackageName)
+                .WithSettings(DocumentPackageSettingsBuilder.NewDocumentPackageSettings().WithInPerson())
+                    .WithSigner(SignerBuilder.NewSignerWithEmail(email1)
+                        .WithFirstName("John1")
+                        .WithLastName("Smith1")
+                        .WithCustomId(signer1Id))
+                    .WithSigner(SignerBuilder.NewSignerWithEmail(email2)
+                        .WithFirstName("John2")
+                        .WithLastName("Smith2")
+                        .WithCustomId(signer2Id))
+                    .WithDocument(DocumentBuilder.NewDocumentNamed(DOCUMENT_NAME)
+                        .FromStream(fileStream1, DocumentType.PDF)
+                        .WithId(documentId)
+                        .WithSignature(SignatureBuilder.SignatureFor(email1)
+                              .OnPage(0)
+                              .AtPosition(100, 100))
+                        .WithSignature(SignatureBuilder.SignatureFor(email1)
+                              .OnPage(0)
+                              .AtPosition(300, 100))
+                        .WithSignature(SignatureBuilder.SignatureFor(email2)
+                              .OnPage(0)
+                              .AtPosition(500, 100))
+                        .WithSignature(SignatureBuilder.SignatureFor(email2)
+                              .OnPage(0)
+                              .AtPosition(700, 100)
+                              .MakeOptional()))
+                    .Build();
+
+            eslClient.
+            packageId = eslClient.CreatePackage(superDuperPackage);
+            eslClient.SendPackage(packageId);
+            sentPackage = eslClient.GetPackage(packageId);
+
+            signer1SignableSignatures = eslClient.ApprovalService.GetAllSignableSignatures(sentPackage, documentId, signer1Id);
+            signer2SignableSignatures = eslClient.ApprovalService.GetAllSignableSignatures(sentPackage, documentId, signer2Id);
+        }
+    }
+}
+

OneSpan/oss.sdk.net

sdk/SDK.Examples/src/SignableOptionalSignaturesExample.cs
@@ -1,0 +1,61 @@
+using System;
+using System.IO;
+using Silanis.ESL.SDK;
+using System.Collections.Generic;
+using Silanis.ESL.SDK.Builder;
+
+namespace SDK.Examples
+{
+    public class SignableOptionalSignaturesExample : SDKSample
+    {
+        public static void Main(string[] args)
+        {
+            new SignableOptionalSignaturesExample().Run();
+        }
+
+        private string signer1Id = "signer1Id";
+        private string signer2Id = "signer2Id";
+        private string documentId = "documentId";
+        private string DOCUMENT_NAME = "First Document";
+
+        public IList<Signature> signer1SignableSignatures, signer2SignableSignatures;
+
+        override public void Execute()
+        {
+            DocumentPackage superDuperPackage = PackageBuilder.NewPackageNamed(PackageName)
+                    .WithSigner(SignerBuilder.NewSignerWithEmail(email1)
+                        .WithFirstName("John1")
+                        .WithLastName("Smith1")
+                        .WithCustomId(signer1Id))
+                    .WithSigner(SignerBuilder.NewSignerWithEmail(email2)
+                        .WithFirstName("John2")
+                        .WithLastName("Smith2")
+                        .WithCustomId(signer2Id))
+                    .WithDocument(DocumentBuilder.NewDocumentNamed(DOCUMENT_NAME)
+                        .FromStream(fileStream1, DocumentType.PDF)
+                        .WithId(documentId)
+                        .WithSignature(SignatureBuilder.SignatureFor(email1)
+                              .OnPage(0)
+                              .AtPosition(100, 100))
+                        .WithSignature(SignatureBuilder.SignatureFor(email1)
+                              .OnPage(0)
+                              .AtPosition(300, 100))
+                        .WithSignature(SignatureBuilder.SignatureFor(email2)
+                              .OnPage(0)
+                              .AtPosition(500, 100))
+                        .WithSignature(SignatureBuilder.SignatureFor(email2)
+                              .OnPage(0)
+                              .AtPosition(700, 100)
+                              .MakeOptional()))
+                    .Build();
+
+            packageId = eslClient.CreatePackage(superDuperPackage);
+            eslClient.SendPackage(packageId);
+            retrievedPackage = eslClient.GetPackage(packageId);
+
+            signer1SignableSignatures = eslClient.ApprovalService.GetAllSignableSignatures(retrievedPackage, documentId, signer1Id);
+            signer2SignableSignatures = eslClient.ApprovalService.GetAllSignableSignatures(retrievedPackage, documentId, signer2Id);
+        }
+    }
+}
+