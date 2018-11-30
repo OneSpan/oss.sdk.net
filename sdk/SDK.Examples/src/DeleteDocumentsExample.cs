116c7ca34596a6183d1b4af5d7d79b7cac74e1d2
mateusz <mateusz.gruszka@vasco.com>
2018-11-30 14:27:33 +0100

    PB-10272 Allow deleting multiple documents in one call few fixes

---
+++ sdk/SDK.Examples/src/DeleteDocumentsExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Silanis.ESL.SDK;
+using Silanis.ESL.SDK.Builder;
+using System.Collections.Generic;
+
+namespace SDK.Examples
+{
+    public class DeleteDocumentsExample : SDKSample
+    {
+        public DocumentPackage RetrievedPackageWithDeletedDocuments { get; set; }
+
+        public const string DOCUMENT1_NAME = "First Document";
+        public const string DOCUMENT2_NAME = "Second Document";
+
+        public static void Main (string [] args)
+        {
+            new DocumentOperationsExample ().Run ();
+        }
+
+        override public void Execute ()
+        {
+
+            // Create a package with documents
+            DocumentPackage BuiltPackage = PackageBuilder.NewPackageNamed (PackageName)
+                .DescribedAs ("This is a package demonstrating document upload")
+                .WithSigner (SignerBuilder.NewSignerWithEmail ("john.smith@email.com")
+                    .WithFirstName ("John")
+                    .WithLastName ("Smith")
+                    .WithTitle ("Managing Director")
+                    .WithCompany ("Acme Inc."))
+                .WithDocument (DocumentBuilder.NewDocumentNamed (DOCUMENT1_NAME)
+                    .WithId ("id1")
+                    .WithDescription ("description1")
+                    .FromStream (fileStream1, DocumentType.PDF)
+                    .Build ())
+                .WithDocument (DocumentBuilder.NewDocumentNamed (DOCUMENT2_NAME)
+                    .WithId ("id2")
+                    .WithDescription ("description2")
+                    .FromStream (fileStream2, DocumentType.PDF)
+                    .Build ())
+                .Build ();
+
+            packageId = eslClient.CreatePackage (BuiltPackage);
+            Console.WriteLine ("package created, id = " + packageId);
+
+            retrievedPackage = eslClient.GetPackage (packageId);
+
+            //This is how you would delete a document from a package
+            IList<String> documents = new List<String> ()
+            {
+                "id1",
+                "id2"
+            };
+            eslClient.PackageService.DeleteDocuments (packageId, documents);
+
+            RetrievedPackageWithDeletedDocuments = eslClient.GetPackage (packageId);
+        }
+    }
+
+}
--- sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/DocumentsDeleteExampleTests.cs
+++ sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/DocumentsDeleteExampleTests.cs
@@ -8,16 +8,18 @@ namespace SDK.Examples
         [Test]
         public void verify ()
         {
-            DocumentsDeleteExample example = new DocumentsDeleteExample ();
+            DeleteDocumentsExample example = new DeleteDocumentsExample ();
             example.Run ();
 
             // Verify if the document was uploaded correctly.
-            Assert.IsNotNull (example.RetrievedPackageWithDocuments.Documents.First (x => x.Name.Equals (DocumentsDeleteExample.DocumentName1)));
-            Assert.IsNotNull (example.RetrievedPackageWithDocuments.Documents.First (x => x.Name.Equals (DocumentsDeleteExample.DocumentName2)));
+            Assert.IsNotNull (example.RetrievedPackage.Documents.First (x => x.Name.Equals (DeleteDocumentsExample.DOCUMENT1_NAME)));
+            Assert.IsNotNull (example.RetrievedPackage.Documents.First (x => x.Name.Equals (DeleteDocumentsExample.DOCUMENT2_NAME)));
+            Assert.AreEqual (example.RetrievedPackage.Documents.Count, 3);
 
             // Verify if the document was deleted correctly.
-            Assert.IsNull (example.RetrievedPackageWithDeletedDocuments.Documents.FirstOrDefault (x => x.Name.Equals (DocumentsDeleteExample.DocumentName1)));
-            Assert.IsNull (example.RetrievedPackageWithDeletedDocuments.Documents.FirstOrDefault (x => x.Name.Equals (DocumentsDeleteExample.DocumentName2)));
+            Assert.IsNull (example.RetrievedPackageWithDeletedDocuments.Documents.FirstOrDefault (x => x.Name.Equals (DeleteDocumentsExample.DOCUMENT1_NAME)));
+            Assert.IsNull (example.RetrievedPackageWithDeletedDocuments.Documents.FirstOrDefault (x => x.Name.Equals (DeleteDocumentsExample.DOCUMENT2_NAME)));
+            Assert.AreEqual (example.RetrievedPackageWithDeletedDocuments.Documents.Count, 1);
         }
 
     }
