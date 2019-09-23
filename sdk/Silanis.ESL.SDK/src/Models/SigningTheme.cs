627536a2cebcbc457d5294de5e3b98f46aa00dde
sohanal1 <alok.sohani@onespan.com>
2019-09-23 11:33:02 -0400

    PB-32206 : Allow customizing Signing Themes in NSC for .Net SDK

---
+++ sdk/Silanis.ESL.SDK/src/Models/SigningTheme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Silanis.ESL.API
+{
+
+    internal class SigningTheme
+    {
+
+        // Fields
+        private Dictionary<string, string> _color = new Dictionary<string, string> ();
+
+        // Accessors
+
+        [JsonProperty ("color")]
+        public Dictionary<string, string> Color {
+            get; set;
+        }
+    }
+}
