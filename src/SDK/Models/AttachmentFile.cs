3335864af54715e0a45d2d47fb6cddc8f8f60036
cartire1 <$Welcome!>
2021-01-13 13:36:05 -0500

    PB-65913 - package > "roles" > "attachmentRequirements" > "files" > "insertDate" be ignored

--- src/SDK/Models/AttachmentFile.cs
+++ src/SDK/Models/AttachmentFile.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace OneSpanSign.API
 {
     public class AttachmentFile
     {
-
-       
+        
+        [JsonProperty("insertDate"), JsonConverter(typeof(MicrosecondEpochConverter))]
         private DateTime insertDate;
 
         [JsonProperty ("id")]
@@ -36,6 +37,27 @@ namespace OneSpanSign.API
         public void SetInsertDate (long value)
         {
             this.insertDate = new DateTime (value);
+        }
+    }
+
+    internal class MicrosecondEpochConverter : DateTimeConverterBase
+    {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteRawValue(((DateTime) value - Epoch).TotalMilliseconds + "");
+        }
+
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+            {
+                return null;
+            }
+
+            return Epoch.AddMilliseconds((long) reader.Value);
         }
     }
 }
