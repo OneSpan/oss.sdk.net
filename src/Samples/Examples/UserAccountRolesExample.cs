c65635b20fea9b845452425cfb6b29ef4f1b854f
XIAO SU <xiao.su@onespan.com>
2021-10-12 22:44:52 -0400

    PB-74141: .NET SDK for API Assign a role to a user.

--- src/Samples/Examples/UserAccountRolesExample.cs
+++ src/Samples/Examples/UserAccountRolesExample.cs
@@ -18,18 +18,18 @@ namespace SDK.Examples
         protected OssClient ossClientWithRoleAndPermission;
         override public void Execute()
         {
+            const string USER_ID = "FakeUserId";
+            const string ACCOUNT_ID = "FakeAccountId";
+            
             //Setup ossclient for role and permission enabled user
             ossClientWithRoleAndPermission = new OssClient(props.Get("api.key.withRolesAndPermission"), props.Get("api.url"), props.Get("webpage.url"), true);
             senderEmail = props.Get("sender.email.withRolesAndPermission");
 
             //Get a role for specific user
-            string userId1 = "qzyGltUv6N87";
-            result1 = ossClientWithRoleAndPermission.AccountService.getUserRoles(userId1);
+            result1 = ossClientWithRoleAndPermission.AccountService.getUserRoles(USER_ID);
             
             //Get a role for specific user under an account
-            string userId2 = "qzyGltUv6N87";
-            string accountId = "FakeAccountId";
-            result2 = ossClientWithRoleAndPermission.AccountService.getUserRoles(userId2, accountId);
+            result2 = ossClientWithRoleAndPermission.AccountService.getUserRoles(USER_ID, ACCOUNT_ID);
             
             //assign AccountRole to a user
             string newAccountRoleName = Guid.NewGuid().ToString();
@@ -44,10 +44,12 @@ namespace SDK.Examples
             fakeAccountRole.Add(accountRole);
             
             OneSpanSign.Sdk.UserAccountRole userAccountRole = UserAccountRoleBuilder.NewUserAccountRole()
-                .WithAccountId("FakeAccountId")
+                .WithAccountId(ACCOUNT_ID)
                 .WithAccountRoles(fakeAccountRole)
-                .WithUserId("FakeUserId")
+                .WithUserId(USER_ID)
                 .Build();
+            
+            ossClientWithRoleAndPermission.AccountService.assignAccountRoleToUser(USER_ID, userAccountRole);
 
         }
 
