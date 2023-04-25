b607b165165908f4c738db9e33b956f4e35b9254
Alok Sohani <alok.sohani@onespan.com>
2023-04-25 09:45:52 -0400

    PB-92761 Add new feature and package settings to .Net SDK for Self Serve Phase 2

--- src/SDK/Builder/AccountPackageSettingsBuilder.cs
+++ src/SDK/Builder/AccountPackageSettingsBuilder.cs
@@ -27,6 +27,11 @@ namespace OneSpanSign.Sdk
         private Nullable<bool> showNseHelp;
         private Nullable<bool> showNseLogoInIframe;
         private Nullable<bool> showNseOverview;
+        private Nullable<bool> title;
+        private Nullable<bool> progressBar;
+        private Nullable<bool> navigator;
+        private Nullable<int> maxAttachmentFiles;
+        private Nullable<int> fontSize;
         
         private AccountPackageSettingsBuilder()
         {
@@ -265,7 +270,56 @@ namespace OneSpanSign.Sdk
             this.showNseOverview = false;
             return this;
         }
+        public AccountPackageSettingsBuilder WithTitle() {
+            this.title = true;
+            return this;
+        }
 
+        public AccountPackageSettingsBuilder WithoutTitle() {
+            this.title = false;
+            return this;
+        }
+        
+        public AccountPackageSettingsBuilder WithProgressBar() {
+            this.progressBar = true;
+            return this;
+        }
+
+        public AccountPackageSettingsBuilder WithoutProgressBar() {
+            this.progressBar = false;
+            return this;
+        }
+        
+        public AccountPackageSettingsBuilder WithNavigator() {
+            this.navigator = true;
+            return this;
+        }
+
+        public AccountPackageSettingsBuilder WithoutNavigator() {
+            this.navigator = false;
+            return this;
+        }
+        
+        public AccountPackageSettingsBuilder WithUnlimitedMaxAttachmentFiles()
+        {
+            this.maxAttachmentFiles = 0;
+            return this;
+        }
+        
+        public AccountPackageSettingsBuilder WithCustomMaxAttachmentFiles(int value)
+        {
+            this.maxAttachmentFiles = value;
+            return this;
+        }
+
+        public AccountPackageSettingsBuilder WithDefaultFontSize() {
+            this.fontSize = 12;
+            return this;
+        }
+        public AccountPackageSettingsBuilder WithCustomFontSize(int value) {
+            this.fontSize = value;
+            return this;
+        }
         public AccountPackageSettings Build() {
             AccountPackageSettings result = new AccountPackageSettings();
             
@@ -292,6 +346,11 @@ namespace OneSpanSign.Sdk
             result.ShowNseHelp = showNseHelp;
             result.ShowNseLogoInIframe = showNseLogoInIframe;
             result.ShowNseOverview = showNseOverview;
+            result.Title = title;
+            result.ProgressBar = progressBar;
+            result.Navigator = navigator;
+            result.MaxAttachmentFiles = maxAttachmentFiles;
+            result.FontSize = fontSize;
                 
             return result;
         }
--- src/Samples.Tests/Tests/AccountSettingsExampleTest.cs
+++ src/Samples.Tests/Tests/AccountSettingsExampleTest.cs
@@ -15,43 +15,67 @@ namespace SDK.Examples
             Assert.IsNotNull(example.defaultAccountSettings.AccountFeatureSettings.AllowInPersonForAccountSenders);
             Assert.IsNotNull(example.defaultAccountSettings.AccountFeatureSettings.Attachments);
             Assert.IsNotNull(example.defaultAccountSettings.AccountFeatureSettings.ConditionalFields);
+            Assert.IsNotNull(example.defaultAccountSettings.AccountFeatureSettings.EnableRecipientHistory);
 
             Assert.IsFalse(example.patchedAccountSettings.AccountFeatureSettings.AllowCheckboxConsentApproval);
             Assert.IsTrue(example.patchedAccountSettings.AccountFeatureSettings.AllowInPersonForAccountSenders);
             Assert.IsFalse(example.patchedAccountSettings.AccountFeatureSettings.Attachments);
             Assert.IsFalse(example.patchedAccountSettings.AccountFeatureSettings.ConditionalFields);
             Assert.IsTrue(example.patchedAccountSettings.AccountFeatureSettings.OverrideRecipientsPreferredLanguage);
+            Assert.IsTrue(example.patchedAccountSettings.AccountFeatureSettings.EnableRecipientHistory);
             
             Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.Ada);
             Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.DeclineButton);
             Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.DefaultTimeBasedExpiry);
             Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.DisableDeclineOther);
+            Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.Title);
+            Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.ProgressBar);
+            Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.Navigator);
+            Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.MaxAttachmentFiles);
+            Assert.IsNotNull(example.defaultAccountSettings.AccountPackageSettings.FontSize);
 
             Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.Ada);
             Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.DeclineButton);
             Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.DefaultTimeBasedExpiry);
             Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.DisableDeclineOther);
+            Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.Title);
+            Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.ProgressBar);
+            Assert.IsTrue(example.patchedAccountSettings.AccountPackageSettings.Navigator);
+            Assert.That(example.patchedAccountSettings.AccountPackageSettings.MaxAttachmentFiles,Is.EqualTo(0));
+            Assert.That(example.patchedAccountSettings.AccountPackageSettings.FontSize,Is.EqualTo(12));
 
             
             Assert.IsNotNull(example.defaultAccountFeatureSettings.AllowCheckboxConsentApproval);
             Assert.IsNotNull(example.defaultAccountFeatureSettings.AllowInPersonForAccountSenders);
             Assert.IsNotNull(example.defaultAccountFeatureSettings.Attachments);
             Assert.IsNotNull(example.defaultAccountFeatureSettings.ConditionalFields);
+            Assert.IsNotNull(example.defaultAccountFeatureSettings.EnableRecipientHistory);
 
             Assert.IsFalse(example.patchedAccountFeatureSettings.AllowCheckboxConsentApproval);
             Assert.IsTrue(example.patchedAccountFeatureSettings.AllowInPersonForAccountSenders);
             Assert.IsFalse(example.patchedAccountFeatureSettings.Attachments);
             Assert.IsFalse(example.patchedAccountFeatureSettings.ConditionalFields);
+            Assert.IsTrue(example.patchedAccountFeatureSettings.EnableRecipientHistory);
 
             Assert.IsNotNull(example.defaultAccountPackageSettings.Ada);
             Assert.IsNotNull(example.defaultAccountPackageSettings.DeclineButton);
             Assert.IsNotNull(example.defaultAccountPackageSettings.DefaultTimeBasedExpiry);
             Assert.IsNotNull(example.defaultAccountPackageSettings.DisableDeclineOther);
+            Assert.IsNotNull(example.defaultAccountPackageSettings.Title);
+            Assert.IsNotNull(example.defaultAccountPackageSettings.ProgressBar);
+            Assert.IsNotNull(example.defaultAccountPackageSettings.Navigator);
+            Assert.IsNotNull(example.defaultAccountPackageSettings.MaxAttachmentFiles);
+            Assert.IsNotNull(example.defaultAccountPackageSettings.FontSize);
 
             Assert.IsTrue(example.patchedAccountPackageSettings.Ada);
             Assert.IsTrue(example.patchedAccountPackageSettings.DeclineButton);
             Assert.IsTrue(example.patchedAccountPackageSettings.DefaultTimeBasedExpiry);
             Assert.IsTrue(example.patchedAccountPackageSettings.DisableDeclineOther);
+            Assert.IsTrue(example.patchedAccountPackageSettings.Title);
+            Assert.IsTrue(example.patchedAccountPackageSettings.ProgressBar);
+            Assert.IsTrue(example.patchedAccountPackageSettings.Navigator);
+            Assert.That(example.patchedAccountPackageSettings.MaxAttachmentFiles,Is.EqualTo(100));
+            Assert.That(example.patchedAccountPackageSettings.FontSize,Is.EqualTo(16));
 
         }
     }
