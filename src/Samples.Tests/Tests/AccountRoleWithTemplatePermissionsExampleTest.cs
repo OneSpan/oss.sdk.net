bcd9568ac4553c14060608c5ebf52c20c8f32c4b
Alok Sohani <alok.sohani@onespan.com>
2024-04-08 18:08:11 -0400

    feat: PB-101661 Add example and tester for Account role with Template Permissions in .Net SDK

---
+++ src/Samples.Tests/Tests/AccountRoleWithTemplatePermissionsExampleTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using OneSpanSign.Sdk;
+
+namespace SDK.Examples
+{
+    [TestFixture()]
+    public class AccountRoleWithTemplatePermissionsExampleTest
+    {
+        [Test()]
+        public void VerifyResult()
+        {
+
+            AccountRoleWithTemplatePermissionsExample example = new AccountRoleWithTemplatePermissionsExample();
+
+            example.Run();
+
+            List<AccountRole> accountRoles1 = null;
+            List<AccountRole> accountRoles2 = null;
+            AccountRole templatePermissionsAccountRole = null;
+            accountRoles1 = example.result1;
+            accountRoles2 = example.result2;
+
+            foreach (OneSpanSign.Sdk.AccountRole forAccountRole in accountRoles2)
+            {
+                if (forAccountRole.Id.Equals(example.templatePermissionsRoleUid))
+                {
+                    templatePermissionsAccountRole = forAccountRole;
+                }
+            }
+            // accountRoles1 has initial Account Roles
+            Assert.GreaterOrEqual(accountRoles1.Count, 1);
+            // accountRoles1 has the new Account Role with Template Permissions
+            Assert.GreaterOrEqual(accountRoles2.Count, 1);
+            Assert.AreEqual(accountRoles1.Count + 1, accountRoles2.Count);
+            
+            Assert.NotNull(templatePermissionsAccountRole);
+            Assert.NotNull(example.templatePermissionsAccountRole);
+            Assert.AreEqual("Account Role with Template Permissions", templatePermissionsAccountRole.Description);
+            
+            Assert.IsTrue(templatePermissionsAccountRole.Permissions.Contains(example.TEMPLATES_PERMISSION));
+            Assert.IsTrue(templatePermissionsAccountRole.Permissions.Contains(example.SHARE_TEMPLATES_PERMISSION));
+
+            templatePermissionsAccountRole = null;
+
+            foreach (OneSpanSign.Sdk.AccountRole forAccountRole in example.result3)
+            {
+                if (forAccountRole.Id.Equals(example.templatePermissionsRoleUid))
+                {
+                    templatePermissionsAccountRole = forAccountRole;
+                }
+            }
+
+            Assert.IsNull(templatePermissionsAccountRole);
+            Assert.NotNull(example.templatePermissionsAccountRole);
+        }
+    }
+}
---
+++ src/Samples/Examples/AccountRoleWithTemplatePermissionsExample.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using OneSpanSign.Sdk;
+using OneSpanSign.Sdk.Builder;
+using System.IO;
+
+namespace SDK.Examples
+{
+    public class AccountRoleWithTemplatePermissionsExample : SDKSample
+    {
+        public static void Main(string[] args)
+        {
+            new AccountRoleWithTemplatePermissionsExample().Run();
+        }
+        public String TEMPLATES_PERMISSION = "templates_layouts.templates";
+        public String SHARE_TEMPLATES_PERMISSION = "templates_layouts.share_templates";
+            
+        public List<AccountRole> result1 = null;
+        public List<AccountRole> result2 = null;
+        public List<AccountRole> result3 = null;
+
+        public String templatePermissionsRoleUid = null;
+        public OneSpanSign.Sdk.AccountRole templatePermissionsAccountRole = null;
+
+        protected OssClient ossClientWithRoleAndPermission;
+        override public void Execute()
+        {
+            //Setup ossclient for role and permission enabled user
+            ossClientWithRoleAndPermission = new OssClient(props.Get("api.key.withRolesAndPermission"), props.Get("api.url"), props.Get("webpage.url"), true);
+            senderEmail = props.Get("sender.email.withRolesAndPermission");
+
+            //Get existing roles
+            result1 = ossClientWithRoleAndPermission.AccountService.getAccountRoles();
+
+            //Create a new role with Template Permissions 
+            string templatePermissionsRoleName = Guid.NewGuid().ToString();
+            OneSpanSign.Sdk.AccountRole accountRole = AccountRoleBuilder.NewAccountRole()
+                .WithName(templatePermissionsRoleName)
+                .WithPermissions(new List<string>() { TEMPLATES_PERMISSION, SHARE_TEMPLATES_PERMISSION })
+                .WithDescription("Account Role with Template Permissions")
+                .WithEnabled(true)
+                .Build();
+
+            ossClientWithRoleAndPermission.AccountService.addAccountRole(accountRole);
+            result2 = ossClientWithRoleAndPermission.AccountService.getAccountRoles();
+
+            foreach (OneSpanSign.Sdk.AccountRole forAccountRole in result2)
+            {
+                if (forAccountRole.Name.Equals(templatePermissionsRoleName))
+                {
+                    templatePermissionsRoleUid = forAccountRole.Id;
+                }
+            }
+
+            //Update the newly created role
+            ossClientWithRoleAndPermission.AccountService.updateAccountRole(templatePermissionsRoleUid, accountRole);
+            result2 = ossClientWithRoleAndPermission.AccountService.getAccountRoles();
+
+            //Get the newly created role
+            templatePermissionsAccountRole = ossClientWithRoleAndPermission.AccountService.getAccountRole(templatePermissionsRoleUid);
+
+            //Delete the newly created role
+            ossClientWithRoleAndPermission.AccountService.deleteAccountRole(templatePermissionsRoleUid);
+            result3 = ossClientWithRoleAndPermission.AccountService.getAccountRoles();
+        }
+    }
+}
