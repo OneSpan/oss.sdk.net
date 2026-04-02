709cc1dc2975787de9adedd6fae17350b24b0963
Maryna Pashkouskaya <maryna.pashkouskaya@onespan.com>
2026-04-02 20:10:35 +0300

    feat: PB-124936 sync with dev

--- src/Samples.Tests/Tests/UpdatePackageExampleTest.cs
+++ src/Samples.Tests/Tests/UpdatePackageExampleTest.cs
@@ -9,6 +9,7 @@ namespace SDK.Examples
     {
         [Test]
         public void verify() {
+            // Asserts that are commented out are so because updating them is not currently supported by the oss server.
         
             UpdatePackageExample example = new UpdatePackageExample();
             example.Run();
