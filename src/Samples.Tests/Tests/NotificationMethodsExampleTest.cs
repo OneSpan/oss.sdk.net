8ec98795b2314063bea3bc441ef0d78fb2e2093d
iryna-onespan <144489853+iryna-onespan@users.noreply.github.com>
2026-04-14 09:15:03 -0400

    feat: PB-121259 support sms as the only notification method for signer (#155)

--- src/Samples.Tests/Tests/NotificationMethodsExampleTest.cs
+++ src/Samples.Tests/Tests/NotificationMethodsExampleTest.cs
@@ -22,6 +22,9 @@ namespace SDK.Examples
             Assert.IsTrue(documentPackage.GetSigner(example.email3).NotificationMethods.Primary.Contains(NotificationMethod.SMS));
           
             Assert.AreEqual(documentPackage.GetSigner(example.email3).NotificationMethods.Phone, "+12042345678");
+
+            Assert.IsTrue(documentPackage.GetSigner(example.email4).NotificationMethods.Primary.Contains(NotificationMethod.SMS));
+            Assert.AreEqual(documentPackage.GetSigner(example.email4).NotificationMethods.Phone, "+12042345690");
         }
     }
 }
