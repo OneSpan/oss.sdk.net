dcfbce4a7780d98d28608eac4211d22d5030ac06
nikrama1 <mahdiye.nikraftar@onespan.com>
2026-08-31 16:52:45 -0400

    feat: PB-130536 expose the clickable-area property- #204 (#5)
    * feat: PB-130536 expose the clickable-area property
    
    * feat: PB-130536 guard clickable-area example tests against null when the flag is disabled
    
    * feat: PB-130536 remove ClickableAreaCloneExample and its test

---
+++ src/SDK/ClickableAreaAlignment.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OneSpanSign.Sdk
+{
+    public enum ClickableAreaAlignment
+    {
+        TOP_LEFT,
+        TOP_CENTER,
+        TOP_RIGHT,
+        CENTER_LEFT,
+        CENTER,
+        CENTER_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_CENTER,
+        BOTTOM_RIGHT
+    }
+}
